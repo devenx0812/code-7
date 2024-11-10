33dd5cd3b698a788d11e7ac6a237dda356ea217f
devenx0812 <devansh.chachra.btech2022@sitpune.edu.in>
2024-11-11 02:31:53 +0530

    Add files via upload

---
+++ 7G.cpp
@@ -0,0 +1,19 @@
+// NAME - GARV NANDWANA
+// PRM - 23070123167
+// EXPERIMENT - 7(G) 
+
+#include<iostream> 
+#include<string>
+using namespace std; 
+
+int main() 
+{
+    string a;
+    cout<<"Enter any word: "; 
+    cin>>a;
+    cout<<"Entered string is "<<a<<"."<<"\n";
+    return 0;
+}
+
+// Enter any word: Environment
+// Entered string is Environment.
---
+++ 7H.cpp
@@ -0,0 +1,19 @@
+// NAME - GARV NANDWANA
+// PRM - 23070123167
+// EXPERIMENT - 7(H) 
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+string a, b;
+cout<<"Enter strings: ";
+cin>>a>>b;
+cout<<"CONCATENATION: "<<a+b;
+
+return 0;
+} 
+
+// Enter strings: Slow
+// Fast
+// CONCATENATION: SlowFast
